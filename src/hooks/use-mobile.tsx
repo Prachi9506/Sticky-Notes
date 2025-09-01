cd7711e5ec4faa85b5d1609b65c85a88201df0e2
Prachi Sharma <prachimansoorpur@gmail.com>
2025-09-02 01:49:50 +0530

    Update use-mobile.tsx

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 const MOBILE_BREAKPOINT = 768
-
+ 
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
@@ -17,6 +17,7 @@ export function useIsMobile() {
   return !!isMobile
 
 }
+
 
 
 
