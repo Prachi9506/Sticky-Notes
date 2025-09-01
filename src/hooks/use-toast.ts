e94e7b630a71a7d7a4bcd501e67378b0d9eff0c9
Prachi Sharma <prachimansoorpur@gmail.com>
2025-09-01 19:48:58 +0530

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -5,7 +5,7 @@ import type {
   ToastProps,
 } from "@/components/ui/toast"
 
-const TOAST_LIMIT = 1 
+const TOAST_LIMIT = 1  
 const TOAST_REMOVE_DELAY = 1000000
 
 type ToasterToast = ToastProps & {
@@ -189,6 +189,7 @@ function useToast() {
 }
 
 export { useToast, toast }
+
 
 
 
