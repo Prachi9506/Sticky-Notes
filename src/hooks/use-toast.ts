22d45d88d09f7f979d9982e0acc4466b0d494878
Prachi Sharma <prachimansoorpur@gmail.com>
2025-09-04 23:24:01 +0530

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -12,7 +12,7 @@ type ToasterToast = ToastProps & {
   id: string 
   title?: React.ReactNode
   description?: React.ReactNode  
-  action?: ToastActionElement
+  action?: ToastActionElement 
 }
 
 const actionTypes = {
@@ -189,6 +189,7 @@ function useToast() {
 }
 
 export { useToast, toast }
+
 
 
 
